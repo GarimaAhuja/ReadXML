a344a66fd93ef029b7e228cafe71a8564c76fbba
GarimaAhuja <garimaahujaishere@gmail.com>
2013-05-10 09:34:36 +0530

    parsing a.xml : file that contains the xml tree

--- parseXML.java
+++ parseXML.java
@@ -1,3 +1,8 @@
+/*
+   This code parses b.xml to get the location of another file a.xml.
+   The file a.xml contains a tree, this code parses a.xml to create that tree.
+ */
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPathConstants;
@@ -18,6 +23,9 @@ public class parseXML
 		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 		docFactory.setNamespaceAware(true);
 		DocumentBuilder builder;
+		
+		
+		//reading b.xml to get the location of a.xml
 		try {
 			builder = docFactory.newDocumentBuilder();
 			Document doc = builder.parse(locationFile);
@@ -28,11 +36,52 @@ public class parseXML
 				for (int i = 0; i < list.getLength(); i++ ) {
 					Element element = (Element) list.item(i);
 					xmlFile = element.getTextContent();
-					System.out.println(xmlFile);
+					//System.out.println(xmlFile);
 				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+		
+		
+
+		//parsing a.xml
+		
+		/*
+		   The while loop updates the XPATH to get the nodes at level 0 then level 1 and so on .... 
+		   it breaks out of the while loop when there are no nodes found at some level.
+		   */
+		
+		XPATH="";
+		String elementAttribute="";
+		String elementValue="";
+		int level = 0;
+		
+		while(true)
+		{
+			XPATH+="/node";
+			try {
+				builder = docFactory.newDocumentBuilder();
+				Document doc = builder.parse(xmlFile);
+				XPathExpression expr = XPathFactory.newInstance().newXPath().compile(XPATH);
+				Object hits = expr.evaluate(doc, XPathConstants.NODESET ) ;
+				if ( hits instanceof NodeList ) {
+					NodeList list = (NodeList) hits ;
+					if(list.getLength()==0)
+						break;
+					System.out.println("\n\nAt level "+level+" ....");
+					for (int i = 0; i < list.getLength(); i++ ) {
+						Element element = (Element) list.item(i);
+						elementAttribute = element.getAttribute("attribute");
+						System.out.println(elementAttribute);
+						elementValue = element.getAttribute("value");
+						System.out.println(elementValue);
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			level++;
 		}
 	}
 }
